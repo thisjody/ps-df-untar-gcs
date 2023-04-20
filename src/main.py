1dd5fc88c9da95b72c386edb3ad1f214a4143925
jody <thisjody@gmail.com>
2023-04-20 11:54:09 -0700

    GcsIO in context or ProcessFile

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 import argparse
 import apache_beam as beam
-from apache_beam.io.gcp.gcsio import GcsIO
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam import PTransform, DoFn
 from apache_beam.io import ReadFromPubSub
 from apache_beam.io.gcp.pubsub import PubsubMessage
+from apache_beam.io.gcp.gcsio import GcsIO
 import logging
 import sys
 
 
+
+
 class ProcessFile(DoFn):
+    def start_bundle(self):
+        from apache_beam.io.gcp.gcsio import GcsIO
+        self.gcs = GcsIO()
+        logging.info(f"GcsIO instance: {self.gcs}")
+
     def process(self, element, *args, **kwargs):
         try:
             file_name = element.attributes['name']
@@ -18,6 +25,7 @@ class ProcessFile(DoFn):
             # ...rest of the code...
         except KeyError as e:
             logging.error(f"Required attribute is missing in the message: {e}")
+
 
 
 class ReadAndProcessFiles(PTransform):
@@ -38,9 +46,6 @@ def main(argv):
     parser.add_argument("--topic", required=True, help="Pub/Sub topic")
     parser.add_argument("--runner", default="DirectRunner", help="Pipeline runner")
     args, _ = parser.parse_known_args(argv)
-
-    gcs = GcsIO()
-
     pipeline_options = PipelineOptions(runner=args.runner, project=args.project)
     with beam.Pipeline(options=pipeline_options) as pipeline:
         _ = pipeline | "Listen to Pub/Sub topic" >> ReadAndProcessFiles(topic=args.topic)
