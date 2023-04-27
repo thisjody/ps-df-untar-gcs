c01f08a87afbea67f0c904f55ed90d0316dfca3c
jody <thisjody@gmail.com>
2023-04-27 15:28:20 -0700

    setup output_topic

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ export TEMP_LOCATION='gs://untar/temp'
 
 # Pub/Sub topic
 export TOPIC='projects/airgcp/topics/ps-df-untar'
+export OUTPUT_TOPIC='projects/airgcp/topics/ps-df-bigquery-ingest'
 
 # Install required packages
 pip install -r ./requirements.txt
@@ -21,9 +22,10 @@ python src/main.py \
   --runner DataflowRunner \
   --project $PROJECT_ID \
   --topic $TOPIC \
+  --output_topic $OUTPUT_TOPIC \
   --region $REGION \
   --temp_location $TEMP_LOCATION \
   --staging_location $STAGING_LOCATION \
-  --job_name ps-df-untar-gcs-test \
+  --job_name ps-df-untar-gcs-notify6 \
   --setup_file ./setup.py \
   --streaming
