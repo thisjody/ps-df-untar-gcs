f4022b88e1f8a5b1d39dc0489c784d320daae58f
jody <thisjody@gmail.com>
2023-05-04 10:59:30 -0700

    creates bq dataset now

--- src/main.py
+++ src/main.py
@@ -14,12 +14,16 @@ class ProcessFile(DoFn):
         from apache_beam.io.gcp.gcsio import GcsIO
         import tarfile
         from io import BytesIO
-        from apache_beam.io.gcp.pubsub import PubsubMessage  # Add this import
+        from apache_beam.io.gcp.pubsub import PubsubMessage
+        from google.cloud import bigquery
+        from google.api_core.exceptions import NotFound
         self.gcs = GcsIO()
         self.tarfile = tarfile
         self.BytesIO = BytesIO
+        self.bigquery = bigquery
+        self.NotFound = NotFound
         logging.info(f"GcsIO instance: {self.gcs}")
-        self.PubsubMessage = PubsubMessage  # Instantiate PubsubMessage
+        self.PubsubMessage = PubsubMessage
 
     def process(self, element, *args, **kwargs):
         try:
@@ -44,7 +48,7 @@ class ProcessFile(DoFn):
 
                         # Extract the top-level directory
                         if top_level_dir is None:
-                            top_level_dir = member.name.split('/')[0]
+                            top_level_dir = member.name.split('/')[0].replace('-', '_')
                             logging.info(f"Top level directory set: {top_level_dir}")  # Log the top-level directory when it is set
                         
                         # Read the file from the tarball
@@ -60,6 +64,22 @@ class ProcessFile(DoFn):
 
             logging.info(f"Contents of {file_name} uploaded to bucket {destination_bucket_name}")
             logging.info(f"Top level directory: {top_level_dir}")  # Log the top-level directory after processing the tar.gz file
+
+            # Connect to BigQuery
+            client = self.bigquery.Client()
+
+            # Use 'top_level_dir' as the dataset ID
+            dataset_id = top_level_dir
+            dataset_ref = client.dataset(dataset_id)
+            dataset = None
+
+            try:
+                dataset = client.get_dataset(dataset_ref)
+                logging.info(f"Dataset '{dataset_id}' already exists.")
+            except self.NotFound:  # Use NotFound instead of self.NotFound
+                logging.info(f"Dataset '{dataset_id}' not found. Creating...")
+                dataset = client.create_dataset(self.bigquery.Dataset(dataset_ref))
+                logging.info(f"Dataset '{dataset_id}' created.")
 
             # Create a message for the second Dataflow pipeline
             message = {
